328c0ec2a666cbb112fcd96e33ae04c95437bf31
bellaatriz <belatriz4157@gmail.com>
2024-09-16 12:04:54 -0300

    Meu Game 3D 16.09

---
+++ Meu Game 3D/Assets/scripts/GameManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+
+    public TextMeshProUGUI hud, MsgVitoria;
+    public int restantes; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        restantes = FindObjectsOfType<Moeda>().Length;
+
+        hud.text = $"Moedas Restantes: {restantes}";
+    }
+
+    public void SubtrairMoedas(int valor)
+    {
+        restante -= valor;
+
+        if (restantes <= 0)
+        {
+            MsgVitoria.text
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
